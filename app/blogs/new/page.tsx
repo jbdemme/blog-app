06d3d39348feb5d422a71e5fefd6acefabebe046
Jonathan Demme <j.b.demme@gmail.com>
2026-06-04 16:57:33 +0200

    refactor: replace TextInput component with inline input fields in new blog form

--- app/blogs/new/page.tsx
+++ app/blogs/new/page.tsx
@@ -5,22 +5,26 @@ const NewBlog = () => {
         <div>
             <h2>Add a new blog</h2>
             <form action={createBlog}>
-                <TextInput title="title" />
-                <TextInput title="author" />
-                <TextInput title="url" />
+                <div>
+                    <label>
+                        Title
+                        <input type="text" name="title" />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Author
+                        <input type="text" name="author" />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        URL
+                        <input type="text" name="url" />
+                    </label>
+                </div>
                 <button type="submit">Add Blog</button>
             </form>
-        </div>
-    )
-}
-
-const TextInput = ({ title }: { title: string }) => {
-    return (
-        <div>
-            <label>
-                {title}
-                <input type="text" name={title} />
-            </label>
         </div>
     )
 }
