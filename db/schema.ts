c2e3e938f4a1b73ec003af7c1cccbc5d568832a3
Jonathan Demme <j.b.demme@gmail.com>
2026-06-05 21:40:53 +0200

    feat: add users table and associate blogs with users via foreign key

--- db/schema.ts
+++ db/schema.ts
@@ -5,5 +5,12 @@ export const blogs = pgTable("blogs", {
     title: text("title").notNull(),
     author: text("author"),
     url: text("url"),
-    likes: integer("likes").default(0).notNull()
+    likes: integer("likes").default(0).notNull(),
+    userId: integer("user_id").references(() => users.id)
+})
+
+export const users = pgTable("users", {
+    id: serial("id").primaryKey(),
+    username: text("username").notNull().unique(),
+    name: text("name").notNull()
 })
