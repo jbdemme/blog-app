b59b527d1d62f4b21942c108a6216a40842bc74f
Jonathan Demme <j.b.demme@gmail.com>
2026-06-05 22:55:11 +0200

    feat: display list of users on the users page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,27 @@
+const users = [
+    {
+        id: 1,
+        username: "jsmith",
+        name: "Jonathan Smith"
+    },
+    {
+        id: 2,
+        username: "gegerion",
+        name: "George Tucker"
+    }
+]
+
 const Users = () => {
     return (
         <div>
             <h2>Users</h2>
+            <ul>
+                {users.map((user) =>
+                    <li key={user.id}>
+                        {user.name}
+                    </li>
+                )}
+            </ul>
         </div>
     )
 }
