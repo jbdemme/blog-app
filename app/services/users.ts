f984fe1e285b33e517e8f8218cfe1d40478a6abe
Jonathan Demme <j.b.demme@gmail.com>
2026-06-05 23:20:39 +0200

    feat: include user blogs in profile page by adding getUserWithBlogs service method

--- app/services/users.ts
+++ app/services/users.ts
@@ -11,3 +11,12 @@ export const getUserByUsername = async (username: string) => {
         where: eq(users.username, username)
     })
 }
+
+export const getUserWithBlogs = async (username: string) => {
+    return db.query.users.findFirst({
+        where: eq(users.username, username),
+        with: {
+            blogs: true
+        }
+    })
+}
--- app/users/[username]/page.tsx
+++ app/users/[username]/page.tsx
@@ -1,9 +1,11 @@
-import { getUserByUsername } from "@/app/services/users"
+import { getUserWithBlogs } from "@/app/services/users"
+import { blogs } from "@/db/schema"
+import Link from "next/link"
 import { notFound } from "next/navigation"
 
 const UserProfile = async ({ params }: { params: Promise<{ username: string }> }) => {
     const { username } = await params
-    const user = await getUserByUsername(username)
+    const user = await getUserWithBlogs(username)
 
     if (!user) {
         notFound()
@@ -12,6 +14,13 @@ const UserProfile = async ({ params }: { params: Promise<{ username: string }> }
     return (
         <div>
             <h2>{user.name}</h2>
+            <ul>
+                {user.blogs.map((blog) =>
+                    <li key={blog.id}>
+                        <Link href={`/blogs/${blog.id}`}>{blog.title}</Link>
+                    </li>
+                )}
+            </ul>
         </div>
     )
 }
