d6847fd8eabe90c7dd72e825679df921054d432f
Jonathan Demme <j.b.demme@gmail.com>
2026-06-10 15:02:40 +0200

    feat: add me page

---
+++ app/me/page.tsx
@@ -0,0 +1,11 @@
+const ProfilePage = () => {
+    return (
+        <div className="bg-mist-800 max-w-2xl mx-auto m-12 p-6 space-y-6">
+            <h2 className="font-bold text-2xl">My Profile</h2>
+            <p><strong>Name:</strong> "JOHN DOE"</p>
+            <p><strong>Username:</strong> "USERNAME"</p>
+        </div>
+    )
+}
+
+export default ProfilePage
