87fdf291be7f3dc7d98f358f1e74248046f2acf0
Jonathan Demme <j.b.demme@gmail.com>
2026-06-12 02:15:15 +0200

    feat: integrate @next/mdx to support rendering the homepage from an MDX file

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-export default function Home() {
+import Homepage from "./homepage.mdx"
+
+const Home = () => {
   return (
-    <div className="px-2 py-8">
-      <h2 className="text-8xl text-white tracking-tighter text-balance mb-8">
-        This is the example blog app.
-      </h2>
-      <p>
-        This blog application is part of the exercises of
-        the mooc.fi full stack course on next.js. I highly
-        recommend it, as it is self taught and great for learning.
-      </p>
+    <div className="markdown">
+      <Homepage />
     </div>
-  );
+  )
 }
+
+export default Home
