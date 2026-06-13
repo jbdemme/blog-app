40d666cc5e92a7b7313ac3f7be0ff247fc020727
Jonathan Demme <j.b.demme@gmail.com>
2026-06-13 15:53:26 +0200

    fix: add missing data-testid attributes to login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -32,7 +32,11 @@ export default function LoginPage() {
     return (
         <div className="mx-auto max-w-2xl flex flex-col items-center gap-8">
             <h2 className="text-2xl font-bold">Login</h2>
-            {error && <p style={{ color: "red" }}>{error}</p>}
+            {error &&
+                <p className="test-red-500" data-testid="error-message">
+                    {error}
+                </p>
+            }
             <form onSubmit={handleSubmit} className="space-y-4">
                 <div>
                     <label className="">
@@ -46,7 +50,11 @@ export default function LoginPage() {
                         <FormInput type="password" name="password" required />
                     </label>
                 </div>
-                <button type="submit" className="bg-amber-400 px-4 py-2 rounded text-background font-bold">
+                <button
+                    type="submit"
+                    data-testid="login-button"
+                    className="bg-amber-400 px-4 py-2 rounded text-background font-bold"
+                >
                     Login
                 </button>
             </form>
